0504e5a8ada328694416a5e3ef5060c120f9e6b3
isabel-nett <86269668+isabel-nett@users.noreply.github.com>
2023-05-24 17:45:22 -0400

    Updated for partner journey

--- dist/api/scripts/constants.js
+++ dist/api/scripts/constants.js
@@ -4,61 +4,10 @@ window.TABS = [
 	{ title: 'API', url: 'api.html' },
 	{ title: 'Certificate', url: 'certificate.html' },
 	{ title: 'Payment Methods', url: 'paymentmethods.html' },
+	{ title: 'Partner Portal Guide', url: 'partners.html' }
 ];
 
 window.NAV_MENU = {
-	index: [
-		{
-			title: 'Welcome',
-			url: '#item-1',
-		},
-		{
-			title: 'API Key',
-			url: '#item-2',
-		},
-		{
-			title: 'Sandbox',
-			url: '#item-3',
-		},
-		{
-			title: 'Additional Integrations',
-			url: '#item-4',
-		},
-		{
-			title: 'GitHub Repository',
-			url: '#item-5',
-		},
-		{
-			title: 'GitHub Discussion',
-			url: '#item-5',
-		},
-	],
-	iframe: [
-		{
-			title: 'Setup',
-			url: '#item-1',
-		},
-		{
-			title: 'Initialize a Triple',
-			url: '#item-2',
-		},
-		{
-			title: 'Creating Payment Tokens',
-			url: '#item-3',
-		},
-		{
-			title: 'Configuration',
-			url: '#item-4',
-		},
-		{
-			title: 'Forms',
-			url: '#item-5',
-		},
-		{
-			title: 'Styling',
-			url: '#item-6',
-		},
-	],
 	api: [
 		{
 			title: 'SDKs',
@@ -102,41 +51,5 @@ window.NAV_MENU = {
 		// 		},
 		// 	],
 		// },
-	],
-	certification: [
-		{
-			title: 'About',
-			url: '#item-1',
-		},
-		{
-			title: 'Setup',
-			url: '#item-2',
-		},
-		{
-			title: 'Resources',
-			url: '#item-3',
-		},
-		{
-			title: 'Tests',
-			url: '#item-4',
-		},
-		{
-			title: 'Certificate',
-			url: '#item-5',
-		},
-	],
-	paymentmethods: [
-		{
-			title: 'Card, ACH, & Crypto',
-			url: '#item-1',
-		},
-		{
-			title: 'Digital Wallets',
-			url: '#item-2',
-		},
-		{
-			title: 'Point-of-Sale',
-			url: '#item-3',
-		},
 	]
 };
